935fd78a47682fd82fd8b24081c80282f553b730
Jaeyoung <98375744+nandagorae@users.noreply.github.com>
2024-12-06 16:14:47 -0500

    Prompt add

--- prompt.py
+++ prompt.py
@@ -41,6 +41,42 @@ PROMPT = {
                 'Improved Answer:')
 }
 
+PROMPT={
+     'Q1': ('Q: Do hamsters provide food for any animals?
+A: Hamsters are prey animals. Prey are food for predators. Thus, hamsters provide food for some animals. So
+the answer is yes.
+Q: Could Brooke Shields succeed at University of Pennsylvania?
+A: Brooke Shields went to Princeton University. Princeton University is about as academically rigorous as the
+University of Pennsylvania. Thus, Brooke Shields could also succeed at the University of Pennsylvania. So the
+answer is yes.
+Q: Yes or no: Hydrogen’s atomic number squared exceeds number of Spice Girls?
+A: Hydrogen has an atomic number of 1. 1 squared is 1. There are 5 Spice Girls. Thus, Hydrogen’s atomic
+number squared is less than 5. So the answer is no.
+Q: Yes or no: Is it common to see frost during some college commencements?
+A: College commencement ceremonies can happen in December, May, and June. December is in the winter, so
+there can be frost. Thus, there could be frost at some commencements. So the answer is yes.
+Q: Yes or no: Could a llama birth twice during War in Vietnam (1945-46)?
+A: The War in Vietnam was 6 months. The gestation period for a llama is 11 months, which is more than 6
+months. Thus, a llama could not give birth twice during the War in Vietnam. So the answer is no.
+Q: Yes or no: Would a pear sink in water?
+A: The density of a pear is about 0.6g/cm3
+, which is less than water. Objects less dense than water float. Thus,
+a pear would float. So the answer is no. Read the given context, and choose the correct answer to the question from options A, B, or C. Respond with a single alphabet.\n'
+                      'Context: {context}\n'
+                     'Question: {question}\n'
+                     '{Option1}\n{Option2}\n{Option3}\n'
+                     'Answer:'),
+     'Q2': ('Answer the multiple choice question with a single letter.\n'
+                 'Context: {context}\n'
+                 'Question: {question}\n'
+                 '{Option1}\n{Option2}\n{Option3}\n'
+                 'Answer:'),
+     'Q3': ('Question: {context} {question}\n'
+                 '{Option1}\n{Option2}\n{Option3}\n'
+                 'Answer:')
+     }
+
+
 MODEL_CARD={
     'T0_3B': 'bigscience/T0_3B',
     'T0_11B': 'bigscience/T0pp',
